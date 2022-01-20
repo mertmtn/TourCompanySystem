1fc681b37fcaccc6bafdf9c963c7adea82cecd2e
MERT METİN <mertmetin1993@gmail.com>
2022-01-20 22:51:38 +0300

    Adding dependency lifecycles on AutofacBusinessModule

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -4,6 +4,8 @@ using Castle.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
 using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -12,6 +14,30 @@ namespace Business.DependencyResolvers.Autofac
         protected override void Load(ContainerBuilder builder)
         { 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterType<GuideManager>().As<IGuideService>().SingleInstance();
+            builder.RegisterType<EfGuideDal>().As<IGuideDal>().SingleInstance();
+
+            builder.RegisterType<LanguageManager>().As<ILanguageService>().SingleInstance();
+            builder.RegisterType<EfLanguageDal>().As<ILanguageDal>().SingleInstance();
+
+            builder.RegisterType<PlaceManager>().As<IPlaceService>().SingleInstance();
+            builder.RegisterType<EfPlaceDal>().As<IPlaceDal>().SingleInstance();
+
+            builder.RegisterType<TourManager>().As<ITourService>().SingleInstance();
+            builder.RegisterType<EfTourDal>().As<ITourDal>().SingleInstance();
+
+            builder.RegisterType<InvoiceManager>().As<IInvoiceService>().SingleInstance();
+            builder.RegisterType<EfInvoiceDal>().As<IInvoiceDal>().SingleInstance();
+
+            builder.RegisterType<TouristManager>().As<ITouristService>().SingleInstance();
+            builder.RegisterType<EfTouristDal>().As<ITouristDal>().SingleInstance();
+
+            builder.RegisterType<NationalityManager>().As<INationalityService>().SingleInstance();
+            builder.RegisterType<EfNationalityDal>().As<INationalityDal>().SingleInstance();
+
+            builder.RegisterType<CountryManager>().As<ICountryService>().SingleInstance();
+            builder.RegisterType<EfCountryDal>().As<ICountryDal>().SingleInstance();
 
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
                 .EnableInterfaceInterceptors(new ProxyGenerationOptions()
--- TourCompany.Web/Program.cs
+++ TourCompany.Web/Program.cs
@@ -1,50 +1,23 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using Business.Abstract;
-using Business.Concrete;
 using Business.DependencyResolvers.Autofac;
-using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Autofac implementation
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureContainer<ContainerBuilder>(builder =>
                 {
                     builder.RegisterModule(new AutofacBusinessModule());
                 });
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<TourCompanyDbContext>();
 
 builder.Services.AddFluentValidation(fv => fv.DisableDataAnnotationsValidation = true);
-
-builder.Services.AddTransient<IGuideService, GuideManager>();
-builder.Services.AddSingleton<IGuideDal, EfGuideDal>();
-
-builder.Services.AddTransient<ILanguageService, LanguageManager>();
-builder.Services.AddSingleton<ILanguageDal, EfLanguageDal>();
-
-builder.Services.AddTransient<IPlaceService, PlaceManager>();
-builder.Services.AddSingleton<IPlaceDal, EfPlaceDal>();
-builder.Services.AddTransient<IPlaceService, PlaceManager>();
-builder.Services.AddSingleton<IPlaceDal, EfPlaceDal>();
-
-builder.Services.AddTransient<ITourService, TourManager>();
-builder.Services.AddSingleton<ITourDal, EfTourDal>();
-
-builder.Services.AddTransient<IInvoiceService, InvoiceManager>();
-builder.Services.AddSingleton<IInvoiceDal, EfInvoiceDal>();
-
-builder.Services.AddTransient<ITouristService, TouristManager>();
-builder.Services.AddSingleton<ITouristDal, EfTouristDal>();
-
-builder.Services.AddTransient<INationalityService, NationalityManager>();
-builder.Services.AddSingleton<INationalityDal, EfNationalityDal>();
-
-builder.Services.AddTransient<ICountryService, CountryManager>();
-builder.Services.AddSingleton<ICountryDal, EfCountryDal>();
 
 var app = builder.Build();
 
