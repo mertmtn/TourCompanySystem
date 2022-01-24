b6cbe678448ed51f77535d492172e0fdeece72a6
MERT METİN <mertmetin1993@gmail.com>
2022-01-25 02:18:35 +0300

    Changing service/manager dependency lifetimes

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -11,32 +11,39 @@ namespace Business.DependencyResolvers.Autofac
 {
     public class AutofacBusinessModule : Module
     {
+        /// <summary>
+        /// Dependency lifetimes
+        /// <br/>
+        /// <b>Transient:</b> InstancePerDependency();<br/>
+        /// <b>Scoped:</b> InstancePerLifetimeScope();<br/>
+        /// <b>Singleton:</b> SingleInstance();<br/>
+        /// </summary> 
         protected override void Load(ContainerBuilder builder)
         { 
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();          
 
-            builder.RegisterType<GuideManager>().As<IGuideService>().SingleInstance();
+            builder.RegisterType<GuideManager>().As<IGuideService>().InstancePerDependency();
             builder.RegisterType<EfGuideDal>().As<IGuideDal>().SingleInstance();
 
-            builder.RegisterType<LanguageManager>().As<ILanguageService>().SingleInstance();
+            builder.RegisterType<LanguageManager>().As<ILanguageService>().InstancePerDependency();
             builder.RegisterType<EfLanguageDal>().As<ILanguageDal>().SingleInstance();
 
-            builder.RegisterType<PlaceManager>().As<IPlaceService>().SingleInstance();
+            builder.RegisterType<PlaceManager>().As<IPlaceService>().InstancePerDependency();
             builder.RegisterType<EfPlaceDal>().As<IPlaceDal>().SingleInstance();
 
-            builder.RegisterType<TourManager>().As<ITourService>().SingleInstance();
+            builder.RegisterType<TourManager>().As<ITourService>().InstancePerDependency();
             builder.RegisterType<EfTourDal>().As<ITourDal>().SingleInstance();
 
-            builder.RegisterType<InvoiceManager>().As<IInvoiceService>().SingleInstance();
+            builder.RegisterType<InvoiceManager>().As<IInvoiceService>().InstancePerDependency();
             builder.RegisterType<EfInvoiceDal>().As<IInvoiceDal>().SingleInstance();
 
-            builder.RegisterType<TouristManager>().As<ITouristService>().SingleInstance();
+            builder.RegisterType<TouristManager>().As<ITouristService>().InstancePerDependency();
             builder.RegisterType<EfTouristDal>().As<ITouristDal>().SingleInstance();
 
-            builder.RegisterType<NationalityManager>().As<INationalityService>().SingleInstance();
+            builder.RegisterType<NationalityManager>().As<INationalityService>().InstancePerDependency();
             builder.RegisterType<EfNationalityDal>().As<INationalityDal>().SingleInstance();
 
-            builder.RegisterType<CountryManager>().As<ICountryService>().SingleInstance();
+            builder.RegisterType<CountryManager>().As<ICountryService>().InstancePerDependency();
             builder.RegisterType<EfCountryDal>().As<ICountryDal>().SingleInstance();
 
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
